a4b609ee749fead6727385d153d9fb20e70c9ce9
Elize Freimanis <elizesf@hotmail.com>
2021-12-29 14:33:27 +0100

    added an h2

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
 
     return (
         <div>
+            <h2>Hej</h2>
             <div className={`menu-ctn${dropDownIsOpen ? ' open' : ''}`}>
                 <img
                     alt=''
