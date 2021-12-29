3a8bb7b16e0ef10d34e978716cdc142af3bcdd4a
Elize Freimanis <elizesf@hotmail.com>
2021-12-29 14:30:24 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import Dropdown from './components/Dropdown';
+import { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import Start from './pages/Start';
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Contact from './pages/Contact';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [dropDownIsOpen, setdropDownIsOpen] = useState(false);
+
+    function toggleDropdown() {
+        setdropDownIsOpen(!dropDownIsOpen);
+    }
+
+    return (
+        <div>
+            <div className={`menu-ctn${dropDownIsOpen ? ' open' : ''}`}>
+                <img
+                    alt=''
+                    src='images/menu1.svg'
+                    className='menu-icon'
+                    onClick={toggleDropdown}
+                />
+                <Dropdown onClick={toggleDropdown} />
+            </div>
+            <Switch>
+                <Route path='/' exact>
+                    <Start />
+                </Route>
+                <Route path='/about'>
+                    <About />
+                </Route>
+                <Route path='/skills'>
+                    <Skills />
+                </Route>
+                <Route path='/contact'>
+                    <Contact />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
