96e5bafec637b348db4f204505dae6d3980de680
Elize Freimanis <elizefreimanis@hotmail.com>
2022-09-05 14:54:35 +0200

    edited text in about

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,4 +1,4 @@
-import './styles.css';
+import "./styles.css";
 
 export default function About() {
     return (
@@ -7,23 +7,20 @@ export default function About() {
             <div className='about-info'>
                 <h1>WHO AM I?</h1>
                 <p>
-                    My name is Elize and i am a front-end developer fresh out of
-                    bootcamp. I strive to be a better developer every day and
-                    looking forward to a fun and exciting carrier in coding! I
-                    did a online bootcamp through Codecademy and although it was
-                    challenging at times, as only coding can be, i loved every
-                    minute of it.
+                    My name is Elize and i am a self-taught Front-End Developer. During the fall of
+                    2021 after a few months of hobby coding i completed the Front-End Engineering
+                    course on CodeCademy and although it was challenging, as only coding can be, i
+                    loved every minute of it. I strive to be a better developer every day and
+                    looking forward to a fun and exciting carrier in coding!
                 </p>
                 <p>
-                    I would concider myself a outgoing and happy person with a
-                    hunger for knowledge. In my free time i like to listen and
-                    dance to music (not professionally but in my livingroom),
-                    meet friends and travel.
+                    I would concider myself a outgoing and happy person with a hunger for knowledge.
+                    In my free time i like to listen and dance to music (not professionally but in
+                    my livingroom), meet friends and family and travel.
                 </p>
                 <p>
-                    I want to start from the bottom and work my way up the
-                    knowledge ladder, hopefully i can become one of the best in
-                    my field one day.
+                    I want to start from the bottom and work my way up the knowledge ladder and
+                    hopefully i can become one of the best in my field one day.
                 </p>
             </div>
         </div>
